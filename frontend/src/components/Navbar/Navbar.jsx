32ef7c31c50663cbf24661ae7017c811111e7075
Mustansir101 <agasi.mustansir@gmail.com>
2025-04-06 23:13:01 +0530

    final changes

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ProfileInfo from "../Cards/ProfileInfo";
 import { useNavigate } from "react-router-dom";
 import SearchBar from "../SearchBar/SearchBar";
+import Logo from "../../assets/Logo.png";
 
 function Navbar({ userInfo, onSearch, getAllNotes, setIsSearch }) {
   const [searchQuery, setSearchQuery] = React.useState("");
@@ -24,13 +25,15 @@ function Navbar({ userInfo, onSearch, getAllNotes, setIsSearch }) {
 
   return (
     <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
-      <h2
-        onClick={() => navigate("/dashboard")}
-        className="text-xl font-medium text-black py-2 cursor-pointer"
-      >
-        Notes
-      </h2>
-
+      <div className="flex items-center">
+        <img src={Logo} alt="" className="w-10 mr-3" />
+        <h2
+          onClick={() => navigate("/dashboard")}
+          className="text-xl font-medium text-black py-2 cursor-pointer"
+        >
+          Notes
+        </h2>
+      </div>
       <SearchBar
         value={searchQuery}
         onChange={(e) => setSearchQuery(e.target.value)}
