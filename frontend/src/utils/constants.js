777d8e6e1ffcb1e96ac4d082c3b3c5657af32595
Mustansir101 <agasi.mustansir@gmail.com>
2025-04-07 23:32:14 +0530

    render 2

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,2 +1,2 @@
-// export const BASE_URL = "https://note-backend-gsoy.onrender.com/";
-export const BASE_URL = "http://localhost:7000/";
+export const BASE_URL = "https://note-app-backend-9ihf.onrender.com";
+// export const BASE_URL = "http://localhost:7000/";
